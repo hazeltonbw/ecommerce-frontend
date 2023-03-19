ba0b6cc9804585ab68baa5fc7a094bfcd44b276a
Brandon Hazelton <hazeltonbw@gmail.com>
2023-03-19 03:59:50 -0700

    css and main files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import Root from "./routes/Root";
+import User from "./routes/User";
+import ErrorPage from "./error-page";
+import Register from "./routes/Register";
+import { userLoader } from "./routes/User";
+// Vite has a different way to access environment variables
+// https://vitejs.dev/guide/env-and-mode.html
+// console.log(import.meta.env.VITE_API_URL);
+const API_URL = import.meta.env.VITE_API_URL;
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "auth/register",
+        element: <Register />,
+      },
+      {
+        path: "users/:user_id",
+        element: <User />,
+        loader: async function ({ params }) {
+          return fetch(`${API_URL}/users/${params.user_id}`);
+        },
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
