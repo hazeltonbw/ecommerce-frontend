d237939af3e11217af619ba2f8a34a6017957f08
Brandon Hazelton <hazeltonbw@gmail.com>
2023-04-24 18:19:45 -0700

    Implemented spinner for loading state

--- src/components/FormSubmitButton.tsx
+++ src/components/FormSubmitButton.tsx
@@ -1,4 +1,7 @@
-type Props = { buttonText: string };
+import { BeatLoader } from "react-spinners";
+import LoadingSpinner from "./LoadingSpinner";
+
+type Props = { buttonText: string; status: string };
 // Submit button is only being used for two forms,
 // login and registration.
 const FormSubmitButton = (props: Props) => {
@@ -6,13 +9,15 @@ const FormSubmitButton = (props: Props) => {
     props.buttonText === "Register"
       ? "Submit registration information"
       : "Submit login information";
+  const isLoading = props.status === "pending";
+
   return (
     <button
       type="submit"
       className="text-white bg-sky-700"
       aria-label={ariaLabel}
     >
-      {props.buttonText}
+      {isLoading ? <LoadingSpinner /> : props.buttonText}
     </button>
   );
 };
