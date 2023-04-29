e9fdacff34efe38e910239b289d102360227e5aa
Brandon Hazelton <hazeltonbw@gmail.com>
2023-04-29 03:51:20 -0700

    Redirect user if they're already logged in

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,6 +1,17 @@
-import LoginForm from "../components/LoginForm";
+import { useEffect } from 'react';
+import LoginForm from '../components/LoginForm';
+import { selectIsLoggedIn } from '../features/auth/authSlice';
+import { useAppSelector } from '../hooks';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const isLoggedIn = useAppSelector(selectIsLoggedIn);
+  const navigate = useNavigate();
+  console.log(isLoggedIn, 'are we logged in? ');
+
+  useEffect(() => {
+    if (isLoggedIn) navigate('/');
+  }, [isLoggedIn]);
   return (
     <div className="flex flex-col flex-1 justify-center items-center p-8 bg-sky-700">
       <LoginForm />
