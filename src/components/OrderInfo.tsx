45a1d3032174122b6d4d270d38ca3b1eb095a7fa
Brandon Hazelton <hazeltonbw@gmail.com>
2023-06-01 01:03:16 -0700

    Implement OrderInfo page

---
+++ src/components/OrderInfo.tsx
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { fetchRecentOrder, getOrderById } from "../features/orders/ordersSlice";
+import { useAppDispatch, useAppSelector } from "../hooks";
+import { selectOrder } from "../features/orders/ordersSlice";
+import type { Order } from "../features/orders/ordersSlice";
+
+const OrderInfo = () => {
+  const { order_id } = useParams();
+  const dispatch = useAppDispatch();
+  const order: Order = useAppSelector(selectOrder);
+  console.log(order_id);
+  console.log(order);
+
+  useEffect(() => {
+    const getOrder = async () => {
+      dispatch(getOrderById(parseInt(order_id)));
+    };
+    const getRecentOrder = async () => {
+      dispatch(fetchRecentOrder());
+    };
+    // This is called when the route /orders/:order_id is matched
+    if (order_id != null) getOrder();
+    // This is called for our Confirmation page
+    else getRecentOrder();
+  }, [order_id]);
+
+  if (order != null)
+    return (
+      <div>
+        <h1 className="text-xl">
+          Your order id is: <span className="font-bold">{`${order.order_id}`}</span>
+        </h1>
+      </div>
+    );
+  else
+    return (
+      <div>
+        <h1>Couldn't find that order, sorry.</h1>
+      </div>
+    );
+};
+
+export default OrderInfo;
