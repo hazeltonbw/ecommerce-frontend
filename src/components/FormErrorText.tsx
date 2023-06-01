b6890738f4fd9ca6c8d56b3983e343c4dc534323
Brandon Hazelton <hazeltonbw@gmail.com>
2023-06-01 00:59:42 -0700

    Implement FormErrorText component

---
+++ src/components/FormErrorText.tsx
@@ -0,0 +1,6 @@
+type Props = { error: boolean; message: string };
+
+const FormErrorText = ({ error, message }: Props) => {
+  return error ? <div className="text-sm text-red-700">{message}</div> : null;
+};
+export default FormErrorText;
