17163bc3568a6e6d9d889b66bc2fc4361dd85b1e
Brandon Hazelton <hazeltonbw@gmail.com>
2023-06-20 00:18:37 -0700

    Change API Base URL for production

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,7 +1,11 @@
 import { AxiosInstance } from "axios";
 import axios from "axios";
+console.log(import.meta.env.MODE, "HERES THE MODE");
 const api: AxiosInstance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL:
+    import.meta.env.MODE === "production"
+      ? import.meta.env.PRODUCTION_BASE_URL
+      : import.meta.env.LOCAL_BASE_URL,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json;charset=UTF-8",
