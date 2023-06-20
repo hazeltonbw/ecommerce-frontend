06ccfa7ee566f7a012741ebd98b76ede56ac263f
Brandon Hazelton <hazeltonbw@gmail.com>
2023-06-20 00:37:12 -0700

    fix typo

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,12 +1,12 @@
 import { AxiosInstance } from "axios";
 import axios from "axios";
 console.log(import.meta.env.PROD, "Running in production mode");
-console.log(import.meta.env.PRODUCTION_BASE_URL, "HERES THE PRODUCTION_BASE_URL");
-console.log(import.meta.env.LOCAL_BASE_URL, "HERES THE LOCAL_BASE_URL");
+console.log(import.meta.env.VITE_PRODUCTION_BASE_URL, "HERES THE PRODUCTION_BASE_URL");
+console.log(import.meta.env.VITE_LOCAL_BASE_URL, "HERES THE LOCAL_BASE_URL");
 const api: AxiosInstance = axios.create({
   baseURL: import.meta.env.PROD
-    ? import.meta.env.PRODUCTION_BASE_URL
-    : import.meta.env.LOCAL_BASE_URL,
+    ? import.meta.env.VITE_PRODUCTION_BASE_URL
+    : import.meta.env.VITE_LOCAL_BASE_URL,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json;charset=UTF-8",
