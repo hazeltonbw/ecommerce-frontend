95dad310db7d8b1decb23e79dad3ff65bf74c002
Brandon Hazelton <hazeltonbw@gmail.com>
2023-06-20 00:27:33 -0700

    debugging API URL

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,11 +1,12 @@
 import { AxiosInstance } from "axios";
 import axios from "axios";
-console.log(import.meta.env.MODE, "HERES THE MODE");
+console.log(import.meta.env.PROD, "Running in production mode");
+console.log(import.meta.env.PRODUCTION_BASE_URL, "HERES THE PRODUCTION_BASE_URL");
+console.log(import.meta.env.LOCAL_BASE_URL, "HERES THE LOCAL_BASE_URL");
 const api: AxiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "production"
-      ? import.meta.env.PRODUCTION_BASE_URL
-      : import.meta.env.LOCAL_BASE_URL,
+  baseURL: import.meta.env.PROD
+    ? import.meta.env.PRODUCTION_BASE_URL
+    : import.meta.env.LOCAL_BASE_URL,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json;charset=UTF-8",
