5f524309efd0472f47824cf489ad84ed61d50cab
Brandon Hazelton <hazeltonbw@gmail.com>
2023-06-25 01:46:47 -0700

    Restructure Products page in a way that is easy on the eyes
    Made images smaller.
    Category title has a thin font size now, to separate it from the product title.
    Prices are bold.
    Product titles are full length now.
    Div with product title, category, and price is now set to grow to fill the remaining space in the parent div. This allows each Product component to be the same size and not look weird.

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { addToCart } from "../features/cart/cartSlice";
 import { useAppDispatch } from "../hooks";
 import QuantityPicker from "./QuantityPicker";
@@ -26,19 +27,19 @@ const Product = ({ product }: Props) => {
       // display an animation or text saying product was added to cart
       //dispatch(addToCart({ product_id: product.product_id, qty }));
       dispatch(addToCart({ ...product, qty: qty }));
-    } catch (err) {}
+    } catch (err) {
+      console.error(err)
+    }
   };
   return (
-    <div className="w-full overflow-hidden rounded shadow-lg sm:max-w-xs">
-      <div className="flex items-center sm:h-[400px]">
-        <img src={product.img} alt={product.title} className="w-full sm:max-h-[400px]" />
-      </div>
-      <div className="px-6 py-4">
-        <h1 className="overflow-hidden overflow-ellipsis whitespace-nowrap font-bold capitalize">
+    <div className="flex flex-col w-full overflow-hidden rounded shadow-lg sm:max-w-xs p-4 justify-center">
+      <img src={product.img} alt={product.title} className="sm:max-h-[200px] object-contain" />
+      <div className="px-6 py-4 flex-1">
+        <h1 className="capitalize">
           {product.title}
         </h1>
-        <p className="capitalize">{product.category}</p>
-        <p className="font-semibold">${product.price}</p>
+        <p className="capitalize font-thin">{product.category}</p>
+        <p className="font-bold">${product.price}</p>
       </div>
       <QuantityPicker qty={1} inCart={false} buttonAction={addProductToCart} />
     </div>
