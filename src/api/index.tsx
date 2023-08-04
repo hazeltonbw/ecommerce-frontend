a9f9b094b8e41746e20a617e61ede57b00bc134d
Brandon Hazelton <hazeltonbw@gmail.com>
2023-08-03 18:03:24 -0700

    Use /api route for API

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -2,9 +2,10 @@ import { AxiosInstance } from "axios";
 import axios from "axios";
 
 const api: AxiosInstance = axios.create({
-  baseURL: import.meta.env.PROD
-    ? import.meta.env.VITE_PRODUCTION_BASE_URL
-    : import.meta.env.VITE_LOCAL_BASE_URL,
+  // baseURL: import.meta.env.PROD
+  //   ? import.meta.env.VITE_PRODUCTION_BASE_URL
+  //   : import.meta.env.VITE_LOCAL_BASE_URL,
+  baseURL: "/api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json;charset=UTF-8",
